f6ccbc7f535e518ccaccadf0111ee2858ba03c23
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-12-07 07:06:10 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,21 @@
+const asyncMatches = (arr, key, callback) => {
+  let count = 0; 
+
+  async.eachSeries(
+    arr,
+    (item, cd) => { 
+      if (item === key) count++; 
+      setImmediate(cb); 
+    },
+    (err) +> { 
+      if (err) {
+          callback(err); 
+      } else {
+          callback(count);
+      }
+    }
+  );
+};
+
+module.exports = asyncMatches; 
 
