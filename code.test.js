0b9b57a4553b8c272dd8e851cbc394fc78150288
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-12-07 07:01:43 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,29 +1,30 @@
-const { assert } = require('console');
 const fs = require('fs');
-const jsc = require('jsverify');
-
+const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
+const async = require('async');
 
-let t = [];
-let counts = [];
 
-t[0] = [0];
-counts[0] = 1;
-
-t[1] = [1,2,1];
-counts[1] = 2;
-
-t[2] = [7,7,7,7,7,7,7];
-counts[2] = 7;
-
-t[3] = [1,2,5,11,1,1];
-counts[3] = 3;
-
-t[4] = [-10,3,1,10,-10,0,-10,-10];
-counts[4] = 4;
-
-for (let i = 0; i < t.length; i++) {
-    countAsync(t[i], t[i][0], function(val) {
-        assert(val === counts[i]);
-    });
-}
+const arg1 = [2, 2, 4, 2];
+asyncMatches(arg1, 2, result => {
+    assert(result === 3);
+});
+const arg2 = [0, 0, 0, 0];
+asyncMatches(arg2, 0, result => {
+    assert(result === 4);
+});
+const arg3 = [500, 23423, 500, 420, 666];
+asyncMatches(arg3, 500, result => {
+    assert(result === 2);
+});
+const arg4 = [0];
+asyncMatches(arg4, 2, result => {
+    assert(result === 0);
+});
+const arg5 = [3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5];
+asyncMatches(arg5, 4, result => {
+    assert(result === 5);
+});
+const arg6 = [1];
+asyncMatches(arg6, 1, result => {
+    assert(result === 1);
+});
