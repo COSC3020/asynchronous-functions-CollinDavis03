071ee3f16dc9f89171e384033fc94784808a9b36
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-12-07 07:14:35 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,37 +1,31 @@
+const fs = require('fs');
 const assert = require('assert');
-const asyncMatches = require('./code.js');
+eval(fs.readFileSync('code.js') + '');
 
-const tests = () => {
-    const arg1 = [2, 2, 4, 2];
-    asyncMatches(arg1, 2, result => {
-        assert.strictEqual(result, 3);
-    });
+// Test cases
+const arg1 = [2, 2, 4, 2];
+asyncMatches(arg1, 2, (result) => {
+    assert(result === 3);
+});
+const arg2 = [0, 0, 0, 0];
+asyncMatches(arg2, 0, (result) => {
+    assert(result === 4);
+});
+const arg3 = [500, 23423, 500, 420, 666];
+asyncMatches(arg3, 500, (result) => {
+    assert(result === 2);
+});
+const arg4 = [0];
+asyncMatches(arg4, 2, (result) => {
+    assert(result === 0);
+});
+const arg5 = [3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5];
+asyncMatches(arg5, 4, (result) => {
+    assert(result === 5);
+});
+const arg6 = [1];
+asyncMatches(arg6, 1, (result) => {
+    assert(result === 1);
+});
 
-    const arg2 = [0, 0, 0, 0];
-    asyncMatches(arg2, 0, result => {
-        assert.strictEqual(result, 4);
-    });
-
-    const arg3 = [500, 23423, 500, 420, 666];
-    asyncMatches(arg3, 500, result => {
-        assert.strictEqual(result, 2);
-    });
-
-    const arg4 = [0];
-    asyncMatches(arg4, 2, result => {
-        assert.strictEqual(result, 0);
-    });
-
-    const arg5 = [3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5];
-    asyncMatches(arg5, 4, result => {
-        assert.strictEqual(result, 5);
-    });
-
-    const arg6 = [1];
-    asyncMatches(arg6, 1, result => {
-        assert.strictEqual(result, 1);
-    });
-};
-
-tests();
 console.log("All tests passed!");
